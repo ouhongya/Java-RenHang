f437a0d6d3453530d0da8700c677441bc91ee735
ct0622 <2206107682@qq.com>
2020-06-13 11:43:58 +0800

    6.8

--- src/main/java/com/renhang/core/apicall/xianwan/pojo/TryApiListRes/TryApiListRes.java
+++ src/main/java/com/renhang/core/apicall/xianwan/pojo/TryApiListRes/TryApiListRes.java
@@ -6,11 +6,29 @@ import java.util.List;
 
 @Data
 public class TryApiListRes {
+    /**
+     * 状态码
+     */
     private Integer status;
+    /**
+     * 返回信息
+     */
     private String msg;
+    /**
+     * 返回信息
+     */
     private String info;
+    /**
+     * 时间戳
+     */
     private String current_timestamp;
+    /**
+     * 手机类型
+     */
     private Integer ptype;
     private List<AdListsRes> items;
+    /**
+     * 用户id
+     */
     private Long userId;
 }
